e14cb49611d21ca36d74ef4d06e98f16e68b0bdb
Zinovev Aleksei <63869857+al-zv@users.noreply.github.com>
2021-10-01 23:20:17 +0500

    Update BaseObject.php

--- BaseObject.php
+++ BaseObject.php
@@ -2,6 +2,7 @@ BaseObject is the base class that implements the property feature.
 BaseObject это базовый класс, реализующий функцию свойства.
 
 A property is defined by a getter method (e.g. getLabel), and/or a setter method (e.g. setLabel). For example, the following getter and setter methods define a property named label:
+Свойство определяется методом getter (равносильно getLabel) и/или методом setter (равносильно setLabel). Например, следующие методы getter и setter определяют свойство label:
 
 private $_label;
 
