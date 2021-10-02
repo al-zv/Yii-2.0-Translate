8c9f444c96faef39cdfa66126b4cdba0b8cf337d
Zinovev Aleksei <63869857+al-zv@users.noreply.github.com>
2021-10-02 23:16:38 +0500

    Update BaseObject.php

--- BaseObject.php
+++ BaseObject.php
@@ -54,3 +54,173 @@ public function __construct($param1, $param2, ..., $config = [])
 }
 That is, a $config parameter (defaults to []) should be declared as the last parameter of the constructor, and the parent implementation should be called at the end of the constructor.
 Параметр $config (по умолчанию []) должен быть определен как последний параметр в конструкторе, и выполнение родительского класса должно вызываться в конце конструктора.
+
+Public Methods
+Открытые методы
+
+Method	Description	Defined By
+Метод Описание Определен в
+__call()	Calls the named method which is not a class method.	yii\base\BaseObject
+__construct()	Constructor.	yii\base\BaseObject
+__get()	Returns the value of an object property.	yii\base\BaseObject
+__isset()	Checks if a property is set, i.e. defined and not null.	yii\base\BaseObject
+__set()	Sets value of an object property.	yii\base\BaseObject
+__unset()	Sets an object property to null.	yii\base\BaseObject
+canGetProperty()	Returns a value indicating whether a property can be read.	yii\base\BaseObject
+canSetProperty()	Returns a value indicating whether a property can be set.	yii\base\BaseObject
+className()	Returns the fully qualified name of this class.	yii\base\BaseObject
+hasMethod()	Returns a value indicating whether a method is defined.	yii\base\BaseObject
+hasProperty()	Returns a value indicating whether a property is defined.	yii\base\BaseObject
+init()	Initializes the object.	yii\base\BaseObject
+Method Details
+__call() public method
+Calls the named method which is not a class method.
+
+Do not call this method directly as it is a PHP magic method that will be implicitly called when an unknown method is being invoked.
+public mixed __call ( $name, $params )
+$name	string	
+The method name
+$params	array	
+Method parameters
+return	mixed	
+The method return value
+throws	yii\base\UnknownMethodException	
+when calling unknown method
+__construct() public method
+Constructor.
+
+The default implementation does two things:
+
+Initializes the object with the given configuration $config.
+Call init().
+If this method is overridden in a child class, it is recommended that
+
+the last parameter of the constructor is a configuration array, like $config here.
+call the parent implementation at the end of the constructor.
+public void __construct ( $config = [] )
+$config	array	
+Name-value pairs that will be used to initialize the object properties
+__get() public method
+Returns the value of an object property.
+
+Do not call this method directly as it is a PHP magic method that will be implicitly called when executing $value = $object->property;.
+
+See also __set().
+public mixed __get ( $name )
+$name	string	
+The property name
+return	mixed	
+The property value
+throws	yii\base\UnknownPropertyException	
+if the property is not defined
+throws	yii\base\InvalidCallException	
+if the property is write-only
+__isset() public method
+Checks if a property is set, i.e. defined and not null.
+
+Do not call this method directly as it is a PHP magic method that will be implicitly called when executing isset($object->property).
+
+Note that if the property is not defined, false will be returned.
+
+See also https://secure.php.net/manual/en/function.isset.php.
+public boolean __isset ( $name )
+$name	string	
+The property name or the event name
+return	boolean	
+Whether the named property is set (not null).
+__set() public method
+Sets value of an object property.
+
+Do not call this method directly as it is a PHP magic method that will be implicitly called when executing $object->property = $value;.
+
+See also __get().
+public void __set ( $name, $value )
+$name	string	
+The property name or the event name
+$value	mixed	
+The property value
+throws	yii\base\UnknownPropertyException	
+if the property is not defined
+throws	yii\base\InvalidCallException	
+if the property is read-only
+__unset() public method
+Sets an object property to null.
+
+Do not call this method directly as it is a PHP magic method that will be implicitly called when executing unset($object->property).
+
+Note that if the property is not defined, this method will do nothing. If the property is read-only, it will throw an exception.
+
+See also https://secure.php.net/manual/en/function.unset.php.
+public void __unset ( $name )
+$name	string	
+The property name
+throws	yii\base\InvalidCallException	
+if the property is read only.
+canGetProperty() public method
+Returns a value indicating whether a property can be read.
+
+A property is readable if:
+
+the class has a getter method associated with the specified name (in this case, property name is case-insensitive);
+the class has a member variable with the specified name (when $checkVars is true);
+See also canSetProperty().
+public boolean canGetProperty ( $name, $checkVars = true )
+$name	string	
+The property name
+$checkVars	boolean	
+Whether to treat member variables as properties
+return	boolean	
+Whether the property can be read
+canSetProperty() public method
+Returns a value indicating whether a property can be set.
+
+A property is writable if:
+
+the class has a setter method associated with the specified name (in this case, property name is case-insensitive);
+the class has a member variable with the specified name (when $checkVars is true);
+See also canGetProperty().
+public boolean canSetProperty ( $name, $checkVars = true )
+$name	string	
+The property name
+$checkVars	boolean	
+Whether to treat member variables as properties
+return	boolean	
+Whether the property can be written
+className() public static method
+Deprecated since 2.0.14. On PHP >=5.5, use ::class instead.
+Returns the fully qualified name of this class.
+public static string className ( )
+return	string	
+The fully qualified name of this class.
+hasMethod() public method
+Returns a value indicating whether a method is defined.
+
+The default implementation is a call to php function method_exists(). You may override this method when you implemented the php magic method __call().
+public boolean hasMethod ( $name )
+$name	string	
+The method name
+return	boolean	
+Whether the method is defined
+hasProperty() public method
+Returns a value indicating whether a property is defined.
+
+A property is defined if:
+
+the class has a getter or setter method associated with the specified name (in this case, property name is case-insensitive);
+the class has a member variable with the specified name (when $checkVars is true);
+See also:
+
+canGetProperty()
+canSetProperty()
+public boolean hasProperty ( $name, $checkVars = true )
+$name	string	
+The property name
+$checkVars	boolean	
+Whether to treat member variables as properties
+return	boolean	
+Whether the property is defined
+init() public method
+Initializes the object.
+
+This method is invoked at the end of the constructor after the object is initialized with the given configuration.
+public void init ( )
